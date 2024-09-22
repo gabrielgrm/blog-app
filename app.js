cabd813cb7f9ad6a8813335a1beeec8ec3d58a99
Gabriel Rodrigues Magalhães <grm@cesar.school>
2024-09-22 19:06:24 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+4// Carregando módulos
+const express = require('express')
+const handlebars = require('express-handlebars')
+const bodyParser = require("body-parser" )
+const app = express()
+// const mongoose = require('mongoose')
+// Configurações
+  // Body Parser 
+    app.use(bodyParser.urlencoded({extended: true}))
+    app.use(bodyParser.json())
+  // Handlebars 
+    app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+    app.set('view engine', 'handlebars')
+  // Mongoose 
+    // Em Breve 
+  //
+// Rotas]
+  app.use('/admin', admin)
+// Outros
+const PORT = 8081
+app.listen(PORT, () => {
+  console.log('Servidor rodando! ')
+})
